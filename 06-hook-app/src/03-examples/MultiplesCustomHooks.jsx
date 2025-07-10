d8b3c4d7386a8d670d0f9efd84ea46456e27ac9f
LangerTwo <adrangel95@gmail.com>
2025-07-10 15:39:26 -0500

    consumir el custom hook

--- 06-hook-app/src/03-examples/MultiplesCustomHooks.jsx
+++ 06-hook-app/src/03-examples/MultiplesCustomHooks.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { useFetch } from '../hooks/useFetch'
 
 export const MultiplesCustomHooks = () => {
-    useFetch();
+    const { data, isLoading, hasError } = useFetch('https://pokeapi.co/api/v2/pokemon/1');
 
     return (
-        <div>MultiplesCustomHooks</div>
+        <>
+            <h1>Información del Pokémon</h1>
+            <hr />
+
+            { isLoading && <h2>Cargando...</h2> }
+            <h2>{ data?.name }</h2>
+        </>
     )
 }
--- 06-hook-app/src/hooks/useFetch.js
+++ 06-hook-app/src/hooks/useFetch.js
@@ -1,24 +1,60 @@
 import React, { useEffect, useState } from 'react'
 
-export const useFetch = () => {
+export const useFetch = ( url ) => {
 
     const [state, setState] = useState({
         data: null,
         isLoading: true,
-        hasError: null,
+        hasError: false,
         error: null,
     });
 
     useEffect(() => {
-
         getFetch()
 
-    }, []);
+    }, [url]);
+
+    const setLoadingState = () => {
+        setState({
+            data: null,
+            isLoading: true,
+            hasError: false,
+            error: null,
+        });
+    }
 
     const getFetch = async () => {
-        const resp = fetch('https://pokeapi.co/api/v2/pokemon/mudkip')
+        setLoadingState();
+
+        const resp = fetch(url)
+
+        // sleep
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
+        if ( !resp ) {
+            setState({
+                data: null,
+                isLoading: false,
+                hasError: true,
+                error: {
+                    code: resp.status,
+                    message: resp.statusText,
+                },
+            });
+            return;
+
+        }
+
         const data = await (await resp).json();
-        console.log(data);
+        setState({
+            data: data,
+            isLoading: false,
+            hasError: false,
+            error: null,
+        });
+        // console.log(data);
+
+        // Manejo del cache
     }
 
 
