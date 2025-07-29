a1a1290093685fc3adb16c9e5d25e606bf1f44be
LangerTwo <adrangel95@gmail.com>
2025-07-29 16:38:58 -0500

    useReducer

--- 06-hook-app/src/main.jsx
+++ 06-hook-app/src/main.jsx
@@ -15,6 +15,8 @@ import './index.css'
 // import { CallbackHook } from './06-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/07-tarea-memo/Padre'
 
+import './08-useReducer/intro-reducer'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Padre />
