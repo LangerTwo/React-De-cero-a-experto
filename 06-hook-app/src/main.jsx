9c1971b0523c8b6765133814218950575f0f293e
LangerTwo <adrangel95@gmail.com>
2025-07-29 12:37:02 -0500

    MemoHook

--- 06-hook-app/src/main.jsx
+++ 06-hook-app/src/main.jsx
@@ -9,10 +9,13 @@ import './index.css'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultiplesCustomHooks } from './03-examples/MultiplesCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLayaoutEffect/Layout'
+// import { Layout } from './05-useLayaoutEffect/Layout'
+// import { Memorize } from './06-memos/Memorize'
+// import { MemoHook } from './06-memos/MemoHook'
+import { CallbackHook } from './06-memos/CallbackHook'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Layout />
+    <CallbackHook />
   </StrictMode>,
 )
