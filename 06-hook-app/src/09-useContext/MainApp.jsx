36e1e06a541d69d56367524d21f58db1400f585c
LangerTwo <adrangel95@gmail.com>
2025-07-31 12:21:18 -0500

    navbar

--- 06-hook-app/src/09-useContext/MainApp.jsx
+++ 06-hook-app/src/09-useContext/MainApp.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Routes, Route, Navigate, Link } from 'react-router-dom';
+import { HomePage } from './HomePage'
+import { AboutPage } from './AboutPage'
+import { LoginPage } from './LoginPagen'
+import { Navbar } from './Navbar';
 
 export const MainApp = () => {
   return (
     <>
         <h1>MainApp</h1>
+        <Navbar />
         <hr />
+
+
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
     </>
   )
 }
--- 06-hook-app/src/main.jsx
+++ 06-hook-app/src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
 import './index.css'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
@@ -18,7 +20,9 @@ import './index.css'
 import { MainApp } from './09-useContext/MainApp'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <MainApp />
-  // </StrictMode>,
+  <BrowserRouter>
+    {/* <StrictMode> */}
+      <MainApp />
+    {/* </StrictMode>, */}
+  </BrowserRouter>
 )
