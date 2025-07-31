a9ad866950828aa6ab9eec13aeb5812c9b0fdf24
LangerTwo <adrangel95@gmail.com>
2025-07-31 11:56:10 -0500

    useContexy mas intalacion de react-router-dom

--- 06-hook-app/src/main.jsx
+++ 06-hook-app/src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultiplesCustomHooks } from './03-examples/MultiplesCustomHooks'
@@ -15,10 +14,11 @@ import './index.css'
 // import { CallbackHook } from './06-memos/CallbackHook'
 // import { Padre } from './07-tarea-memo/07-tarea-memo/Padre'
 // import './08-useReducer/intro-reducer'
-import { TodoApp } from './08-useReducer/TodoApp'
+// import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <TodoApp />
+    <MainApp />
   // </StrictMode>,
 )
