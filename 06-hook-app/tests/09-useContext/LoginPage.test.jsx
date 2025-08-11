5224116c4b9d7d7858169c65ce5560264c4c86d9
LangerTwo <adrangel95@gmail.com>
2025-08-11 12:20:37 -0500

    test de fuciones del context en LoginPage

---
+++ 06-hook-app/tests/09-useContext/LoginPage.test.jsx
@@ -0,0 +1,51 @@
+/**
+ * @jest-environment jsdom
+*/
+
+import { fireEvent, render, screen } from "@testing-library/react";
+import { LoginPage } from "../../src/09-useContext/LoginPagen";
+import { UserContext } from "../../src/09-useContext/context/UserContext";
+
+
+describe('Pruebas en <LoginPage />', () => {
+
+
+    test('debe de mostrar el componente sin el usuario', () => {
+        
+        render(
+            <UserContext.Provider value={{ user: null }}>
+                <LoginPage />
+            </UserContext.Provider>
+        );
+        
+        const preTag = screen.getByLabelText('pre');
+        // console.log(preTag.innerHTML);
+        expect(preTag.innerHTML).toBe('null');
+
+    });
+
+
+    test('debe de llamar el setUser cuando se hace click en el boton', () => {
+      
+        const setUserMock = jest.fn();
+
+        render(
+            <UserContext.Provider value={{ user: null, setUser: setUserMock }}>
+                <LoginPage />
+            </UserContext.Provider>
+        );
+
+        const button = screen.getByRole('button');
+        fireEvent.click(button);
+
+        expect( setUserMock ).toHaveBeenCalledWith({
+            id: 123,
+            name: 'Leon Nava',
+            email: 'test@test.com'
+        });
+
+    });
+    
+    
+
+});
