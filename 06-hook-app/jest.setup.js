c03604ff6b93b1f08221ec58e58f8ae89b402638
LangerTwo <adrangel95@gmail.com>
2025-08-11 12:33:29 -0500

    test generales en AppRouter

--- 06-hook-app/jest.setup.js
+++ 06-hook-app/jest.setup.js
@@ -1,2 +1,6 @@
 // import '@testing-library/jest-dom';
 import 'whatwg-fetch';
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
