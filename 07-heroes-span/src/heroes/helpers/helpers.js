0f3dced7ad2d67861db53227f6ff0657855675e0
LangerTwo <adrangel95@gmail.com>
2025-08-13 16:40:54 -0500

    lista de heroes

--- 07-heroes-span/src/heroes/helpers/helpers.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const helpers = () => {
-  return (
-    <div>helpers</div>
-  )
-}
--- 07-heroes-span/src/heroes/pages/DcPage.jsx
+++ 07-heroes-span/src/heroes/pages/DcPage.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { HeroList } from '../components'
 
 export const DcPage = () => {
   return (
-    <div>DcPage</div>
+    <>
+      <h1>Dc Comics</h1>
+      <hr />
+
+      <HeroList publisher='DC Comics' />
+    </>
   )
 }
