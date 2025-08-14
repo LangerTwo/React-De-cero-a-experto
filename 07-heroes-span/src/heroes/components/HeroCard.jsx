13746df37e2e86b0483cdf67d71ae7f85f736c49
LangerTwo <adrangel95@gmail.com>
2025-08-14 11:52:54 -0500

    tajera heroes parte 2

--- 07-heroes-span/src/heroes/components/HeroCard.jsx
+++ 07-heroes-span/src/heroes/components/HeroCard.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+const CharactesByHero = ({ alter_ego, characters }) => {
+    if ( alter_ego === characters ) return (<></>)
+
+    return <p>{ characters }</p>
+}
 
 export const HeroCard = ({
     id,
@@ -10,6 +17,8 @@ export const HeroCard = ({
 }) => {
 
     const heroImageUrl = `/assets/heroes/${id}.jpg`;
+
+    // const charactesByHero = (<p>{ characters }</p>)
 
     return (
         <div className='col'>
@@ -24,9 +33,19 @@ export const HeroCard = ({
                             <h5 className="card-title">{ superhero }</h5>
                             <p className="card-text">{ alter_ego }</p>
 
-                            {
-                                ( alter_ego !== characters ) && <p className="text-muted">{ characters }</p>
-                            }
+                            {/* {
+                                ( alter_ego !== characters ) && charactesByHero
+                            } */}
+
+                            <CharactesByHero alter_ego={ alter_ego } characters={ characters } />
+
+                            <p className='card-text'>
+                                <small className='text-muted'>{ first_appearance }</small>
+                            </p>
+
+                            <Link to={`/hero/${id}`}>
+                                Mas...
+                            </Link>
                         </div>
                     </div>
                 </div>
