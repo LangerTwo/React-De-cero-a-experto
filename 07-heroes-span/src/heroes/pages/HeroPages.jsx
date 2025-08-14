26c057c02fd543c3704c4649c1ffba316e6f106c
LangerTwo <adrangel95@gmail.com>
2025-08-14 13:52:45 -0500

    Hero page

--- 07-heroes-span/src/heroes/pages/HeroPages.jsx
+++ 07-heroes-span/src/heroes/pages/HeroPages.jsx
@@ -11,10 +11,27 @@ export const HeroPages = () => {
   // console.log(hero);
 
   if (!hero) {
-    return <Navigate to="/" />
+    return <Navigate to="/marvel" />
   }
 
   return (
-    <h1>HeroPages</h1>
+    <div className='row mt-5'>
+      <div className="col-4">
+        <img 
+          src={ `../assets/heroes/${hero.id}.jpg` } 
+          alt={ hero.superhero }
+          className='img-thumbnail'
+        />
+      </div>
+
+      <div className="col-8">
+        <h3>{ hero.superhero }</h3>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'> <b>Alter ego:</b> { hero.alter_ego }</li>
+          <li className='list-group-item'> <b>Publisher:</b> { hero.publisher }</li>
+          <li className='list-group-item'> <b>First appearance:</b> { hero.first_appearance }</li>          
+        </ul>
+      </div>
+    </div>
   )
 }
